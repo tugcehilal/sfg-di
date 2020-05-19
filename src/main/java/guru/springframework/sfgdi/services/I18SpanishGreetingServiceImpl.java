569f2498ba4bf693b6e2c8c8cc0a46e4950a1731
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-05-19 22:34:11 +0300

    Dependency Injection with Spring

--- src/main/java/guru/springframework/sfgdi/services/I18SpanishGreetingServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/services/I18SpanishGreetingServiceImpl.java
@@ -3,7 +3,8 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+//Application.properties de bir şey yoksa burdaki default çalışır.
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18SpanishGreetingServiceImpl implements GreetingService {
     @Override
