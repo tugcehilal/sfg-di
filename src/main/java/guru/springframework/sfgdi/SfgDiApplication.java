ee18499d1836e15e1aaed57829ddb4ae30045870
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-05-19 22:08:37 +0300

    Spring Profiles

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx =SpringApplication.run(SfgDiApplication.class, args);
+
+		I18Controller i18Controller = (I18Controller)ctx.getBean("i18Controller");
+		System.out.println(i18Controller.sayHello());
 		MyController myController = (MyController) ctx.getBean("myController");
 		//beans name is the class name just the first letter small
 
