91f9d93032cce783963babefdc6c0c0971a1a36f
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-05-19 19:15:12 +0300

    sec 35 Dependency Injection using Spring Framework

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,10 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,8 +18,18 @@ public class SfgDiApplication {
 		//beans name is the class name just the first letter small
 		String greeting = myController.sayHello();
 		System.out.println(greeting);
+		System.out.println("----Property");
 
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController)ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
 		//there is no web server configured.
+		System.out.println("----Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("----Constructor");
+		ConstructorInjectedController ConstructorInjectedController = (ConstructorInjectedController)ctx.getBean("constructorInjectedController");
+		System.out.println(ConstructorInjectedController.getGreeting());
 	}
 
 
--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,11 +1,14 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
 
 //Best practise
+@Controller
 public class ConstructorInjectedController {
 
+    //Autowired a artık gerek yok
     private final GreetingService greetingService;
 
     public ConstructorInjectedController(GreetingService greetingService) {
